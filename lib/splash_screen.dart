c1a1e4429296507de9841c790cd82d13365ab8ea
Loknath Giri <giriloknath411@gmail.com>
2024-08-18 21:40:42 +0530

    21:35

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
     Timer(const Duration(seconds: 2), (){
       Navigator.pushReplacement(context,
           MaterialPageRoute(
-              builder:(context) => const MyHomePage(title: "Flutter demo Home page")));
+              builder:(context) => const HomeScreen()));
     });
   }
 
